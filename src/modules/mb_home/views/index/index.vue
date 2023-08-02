666c70cd4247cb19fed4ef7a23774cd2e2549db4
dreamjser <dreamjser@gmail.com>
2023-08-02 14:24:41 +0800

    fix: request请求更新

--- src/modules/mb_home/views/index/index.vue
+++ src/modules/mb_home/views/index/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <Button>12</Button>
+    <PasswordInput />
+    <Dialog />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { Button } from 'vant'
+import { Button, PasswordInput, Dialog } from 'vant'
 
 App.interface.toast({
   message: '1',
