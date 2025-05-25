648fa2f1912d9cf218f438654a63bc5f9d14f922
dreamjser <dreamjser@gmail.com>
2025-05-25 22:57:54 +0800

    feat: 模板html位置修改

--- src/common/app/index.ts
+++ src/common/app/index.ts
@@ -1,11 +1,11 @@
 import request from '../request'
 import _interface from './interface'
-import router from '../router'
+// import router from '../router'
 
 const app = {
   request,
   interface: _interface,
-  router,
+  // router,
 }
 
 window.App = app
